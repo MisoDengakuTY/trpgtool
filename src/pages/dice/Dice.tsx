591c7137c99e708c56c0bac8e6f96927d085b159
_cyan13 <kima@kima>
2020-06-19 18:09:48 +0900

    Solve Conflict

--- src/pages/dice/Dice.tsx
+++ src/pages/dice/Dice.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import {Button} from 'rsuite'
 
 import DiceLog from './DiceLog';
 import DiceRoll from './DiceRoll';
-
+import { useState } from 'react';
+import DiceLogDB from './model/diceLogDB';
 
 
 const Dice = () => {
+    const [logs, setLogs] = useState<DiceLogDB[]>([])
+    const [newToPast, setOrdering] = useState<boolean>(false);
+
     return (
         <div className="Dice">
         <h1> Dice </h1>
-        <DiceRoll />
-        <DiceLog />
+        <DiceRoll onRoll={(log: DiceLogDB) => {setLogs(logs.concat([log]))}} />
+        <Button onClick={() => setOrdering(!newToPast)}>{newToPast ? "古い順" : "新しい順"}</Button>
+        <DiceLog logs={logs} newToPast={newToPast}/>
         </div>
     )
 }
--- src/pages/dice/DiceRoll.tsx
+++ src/pages/dice/DiceRoll.tsx
@@ -1,56 +1,66 @@
 import React, { useState } from 'react';
 import {Button,ButtonToolbar,Modal} from 'rsuite';
 import diceFunc from './diceFunc'
+import diceLogDB from './model/diceLogDB';
+import Dice from './Dice';
 
 interface stateType{
   size : undefined|'xs'
   show : boolean
 }
 
+interface DiceRollProp {
+  onRoll : (log: diceLogDB) => void; // ダイスが振られた時に呼ばれるハンドラ
+}
 
-const DiceRoll = () =>{
+const DiceRoll = (prop: DiceRollProp) => {
   
   const [state, setState] = useState<stateType>({size:undefined,show:false});
   const [dice, setDice] = useState<number[]>(Array(0));
+
+  // ダイスロール方式を受け取ってその方式に対応したアクション(() => void)を返す関数
+  const rollHandlerGenerator: ((diceCount: number, diceSize: number) => (() => void)) = 
+    (diceCount: number, diceSize: number) => 
+      () => {
+        setState({size:'xs',show:true});
+        const rollResults = diceFunc(diceCount, diceSize);
+        const now = new Date();
+
+        console.log(rollResults);
+
+        const logObject = new diceLogDB(now.getTime(), diceCount, diceSize, rollResults, now);
+        prop.onRoll(logObject);
+        setDice(rollResults);
+      };
   
   return (
-        <div className="modal-container">
-          <ButtonToolbar>
-            <Button size="lg" onClick={() => {
-            setState({size:'xs',show:true})
-            setDice(diceFunc(1,2))}
-            }>
-              1d2
-          </Button>
-          <Button size="lg" onClick={() => {
-            setState({size:'xs',show:true})
-            setDice(diceFunc(1,3))}
-            }>
-              1d3
-          </Button>
-          
-          <Button size="lg" onClick={() => {
-            setState({size:'xs',show:true})
-            setDice(diceFunc(2,3))}
-            }>
-              2d3
-          </Button>
+    <div className="modal-container">
+      <ButtonToolbar>
+        <Button size="lg" onClick={rollHandlerGenerator(1, 2)}>
+          1d2
+        </Button>
+        <Button size="lg" onClick={rollHandlerGenerator(1, 3)}>
+          1d3
+        </Button>
+        <Button size="lg" onClick={rollHandlerGenerator(2, 3)}>
+          2d3
+        </Button>
+      </ButtonToolbar>
 
-          </ButtonToolbar>
-          <Modal size={state.size} show={state.show} onHide={() => setState({size:undefined,show:false})}>
-            <Modal.Header>
-              <Modal.Title>Dice Roll</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-            {dice.join(", ")} => {dice.reduce(function (accumulator, currentValue) {return accumulator + currentValue}, 0)}
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={() => setState({size:undefined,show:false})} appearance="primary">
-                Ok
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </div>
+      <Modal size={state.size} show={state.show} onHide={() => setState({size:undefined,show:false})}>
+        <Modal.Header>
+          <Modal.Title>Dice Roll</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {dice.join(", ")} {"=>"} {dice.reduce(function (accumulator, currentValue) {return accumulator + currentValue}, 0)}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => setState({size:undefined,show:false})} appearance="primary">
+            Ok
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
   
