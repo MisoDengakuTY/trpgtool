21b0170c787c6c4d57cb6946d3ee2edb5bdd3cff
_cyan13 <kima@kima>
2020-07-20 17:30:35 +0900

    Add coc skill diceroll (refactoring is planed)

--- src/pages/coc/CoC.tsx
+++ src/pages/coc/CoC.tsx
@@ -6,7 +6,7 @@ import Skill from './Skill';
 import Combat from './Combat';
 import Belongings from './Belongings';
 import Personal from './Personal';
-
+import {createDicerollHandler, DiceResultModal, diceState, diceRollHandler} from '../dice/DiceRoll'
 
 import './CoC.css';
 
@@ -21,29 +21,39 @@ let db = new Database({ filename: "./../../localdb/cocCharacters.db", autoload: 
 
 interface ContextType  {
     characterdata : CoCCharacterType,
-    setCharacterData : React.Dispatch<React.SetStateAction<CoCCharacterType>>
+    setCharacterData : React.Dispatch<React.SetStateAction<CoCCharacterType>>,
+    diceRollHandler: diceRollHandler
 }
 
 
 export const CharacterData = createContext({} as ContextType)
 
 const CoC = () => {
+
+    // ダイスロール結果表示用のState
+    const [state, setState] = useState<diceState>({show:false, diceResult: []});
+    const diceRollHandler = createDicerollHandler((res, log, text, limit) => {
+        setState({show: true, diceLog: log, diceResult: res, text: text, limit : limit})
+    });
+
     const [characterdata,setCharacterData] = useState(initialState)
-    const value = {characterdata,setCharacterData};
+    const value = {characterdata,setCharacterData,diceRollHandler};
     
     return (
-        <div className="CoC">
-        <h1> CoC </h1>
-        <CharacterData.Provider value={value} >
-        <Status />
-        <San />
-        <Skill/>
-        <Combat />
-        <Belongings />
-        <Personal />
-        </CharacterData.Provider>
+        <div className="modal-container">
+            <div className="CoC">
+                <h1> CoC </h1>
+                <CharacterData.Provider value={value} >
+                    <Status />
+                    <San />
+                    <Skill />
+                    <Combat />
+                    <Belongings />
+                    <Personal />
+                    <DiceResultModal size="xs" resultStyle="(xDx)=>numbers(sum)<=(limit):(result)" diceState={state} onHide={() => setState({...state, show: false})} />
+                </CharacterData.Provider>
+            </div>
         </div>
-
     )
 }
 
