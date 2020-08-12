e84fefb4a58280d5995bf3d5c7f1bb251b757419
MisoDengakuTY <yonetomo1116otaku@gmail.com>
2020-08-12 14:56:59 +0900

    修正

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ const cors = require('koa-cors');
 const serve = require("koa-static");
 const mount = require("koa-mount");
 const HttpStatus = require("http-status");
+var mongoose = require('mongoose');
+
+
+/////////////////////////test json file//////////////////////////////////////
+const fs = require('fs');
+const jsonObject = JSON.parse(fs.readFileSync('./test.json', 'utf8'));
+/////////////////////////////////////////////////////////////////////////////
+
 
 const app = new Koa();
 
@@ -17,10 +25,11 @@ app.use(cors());
 
 const router = new Router();
 
-router.get("/coc/:id",async (ctx,next)=>{
+router.get("/coc",async (ctx,next)=>{
     const books = ["Speaking javascript", "Fluent Python", "Pro Python", "The Go programming language"];
     ctx.status = HttpStatus.OK;
-    ctx.body = ctx.params.id;
+    ctx.type = 'json';
+    ctx.body = jsonObject;
     await next();
 });
 
