910d45a11ed1e0e8c88499fa9d090b9af620429c
MisoDengakuTY <yonetomo1116otaku@gmail.com>
2020-08-12 12:47:21 +0900

    designed by  Koa

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const Koa = require("koa");
+const BodyParser = require("koa-bodyparser");
+const Router = require("koa-router");
+const Logger = require("koa-logger");
+const cors = require('koa-cors');
+const serve = require("koa-static");
+const mount = require("koa-mount");
+const HttpStatus = require("http-status");
+
+const app = new Koa();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(BodyParser());
+app.use(Logger());
+app.use(cors());
+
+const router = new Router();
+
+router.get("/coc/:id",async (ctx,next)=>{
+    const books = ["Speaking javascript", "Fluent Python", "Pro Python", "The Go programming language"];
+    ctx.status = HttpStatus.OK;
+    ctx.body = ctx.params.id;
+    await next();
+});
+
+router.get("/shinobigami/:id",async (ctx,next)=>{
+    const books = ["Speaking javascript", "Fluent Python", "Pro Python", "The Go programming language"];
+    ctx.status = HttpStatus.OK;
+    ctx.body = ctx.params.id;
+    await next();
+});
+
+router.get("/insane/:id",async (ctx,next)=>{
+    const books = ["Speaking javascript", "Fluent Python", "Pro Python", "The Go programming language"];
+    ctx.status = HttpStatus.OK;
+    ctx.body = ctx.params.id;
+    await next();
+});
+
+
+
+
+app.use(router.routes()).use(router.allowedMethods());
+  
+
+app.listen(PORT, function () {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/", PORT, PORT);
+});
